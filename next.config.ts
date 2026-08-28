0caad954d3deaaabceeee8bce91034667c1350e9
Davi Cagnato <betocagnato@gmail.com>
2026-08-28 17:01:34 -0300

    fix: config para export estatico

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // Necessário caso use componentes de imagem do Next.js de forma estática
+  },
 };
 
 export default nextConfig;
